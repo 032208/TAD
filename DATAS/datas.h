953b5337dafc5f7acdbc77a3b807a5b0e47c30de
Beatriz Andrade <beatriz.anddradesilva@gmail.com>
2024-07-20 16:56:23 -0300

    DATAS/datas.c

---
+++ DATAS/datas.h
@@ -0,0 +1,8 @@
+typedef struct
+{
+    unsigned int *dia, *mes, *ano;
+    int size;
+}Data;
+
+//criar uma data de forma dinâmica 
+Data * novaData(unsigned int dia, unsigned int mes, unsigned int ano);
