532968bd0b24004343d790a599287289ee8508e4
Beatriz Andrade <beatriz.anddradesilva@gmail.com>
2024-07-20 16:59:49 -0300

    CONJUNTOS/conjuntos.c CONJUNTOS/conjuntos.h CONJUNTOS/main.c DATAS/datas.c DATAS/main.c

---
+++ CONJUNTOS/main.c
@@ -0,0 +1,12 @@
+//chama as funções
+#include <stdio.h>
+#include "conjuntos.h"
+
+int main(){
+
+    Conjuntos * A = novo_conjunto();
+    Conjuntos * B = novo_conjunto();
+    Conjuntos * C = novo_conjunto();
+
+    return 0;
+}
---
+++ DATAS/datas.c
@@ -0,0 +1,8 @@
+#include "datas.h"
+#include <stdlib.h>
+
+Data * novaData(unsigned int dia, unsigned int mes, unsigned int ano){
+    Data * nova = (Data*) calloc (1, sizeof(Data));
+    
+    return novaData;
+}
